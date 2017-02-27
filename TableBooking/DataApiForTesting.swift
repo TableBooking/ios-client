3317d5d0d7dac71c508fe2ea59b00ec2da8e4bc7
nikitakirichek <nikita.kirichek@gmail.com>
2017-02-27 13:18:35 +0200

    Add json files for testing

---
+++ TableBooking/DataApiForTesting.swift
@@ -0,0 +1,9 @@
+//
+//  DataApiForTesting.swift
+//  TableBooking
+//
+//  Created by Nikita Kirichek on 2/26/17.
+//  Copyright © 2017 Nikita Kirichek. All rights reserved.
+//
+
+import Foundation
